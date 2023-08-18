350f7dda1f3e4f4e2f34e6f307d99738049bda75
anas misbah <anas.misbahuddin4869@gmail.com>
2023-08-18 22:15:16 +0000

    add config databse and init in main

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB()  {
+
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+	os.Getenv("DB_ROOT"),
+	os.Getenv("DB_PASSWORD"),
+	os.Getenv("DB_HOST"),
+	os.Getenv("DB_PORT"),
+	os.Getenv("DB_NAME"))
+
+	var e error
+	DB,e = gorm.Open(mysql.Open(dsn),&gorm.Config{})
+	if e!=nil {
+		panic(e)
+	}
+}
